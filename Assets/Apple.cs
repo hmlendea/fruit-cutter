caf769c2a3629d1a104d773b15a3f1efef662a70
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-01-11 23:09:04 +0200

    Created the apple and fruit spawner scripts

---
+++ Assets/Apple.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using System.Runtime.InteropServices;
+
+public class Apple : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject splashObject;
+
+    private GUIText scoreGuiText;
+
+    private Vector3 randomPos = new Vector3(
+                                    Random.Range(-1, 1),
+                                    Random.Range(0.3f, 0.7f),
+                                    Random.Range(-6.5f, -7.5f));
+
+    /// <summary>
+    /// Initialise this apple.
+    /// </summary>
+    void Start()
+    {
+        scoreGuiText = GetComponent<GUIText>(GameObject.Find("Score Text"));
+    }
+
+    /// <summary>
+    /// Update this apple.
+    /// </summary>
+    void Update()
+    {
+        if (gameObject.transform.position.y < -36)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Raises the collision enter event.
+    /// </summary>
+    /// <param name="other">Other.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Line")
+        {
+            Camera.main.GetComponent<AudioSource>().Play();
+            Destroy(gameObject);
+
+            Instantiate(splashObject, randomPos, transform.rotation);
+
+            int currentScore = int.Parse(scoreGuiText.text);
+            scoreGuiText.text = (currentScore + 1).ToString();
+        }
+    }
+}
---
+++ Assets/FruitSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject appleObject;
+
+    private Vector3 throwForce = new Vector3(0, 18, 0);
+
+    /// <summary>
+    /// Start this fruit spawner.
+    /// </summary>
+    void Start()
+    {
+        InvokeRepeating("SpawnFruit", 0.5f, 6);
+    }
+
+    void SpawnFruit()
+    {
+        for (byte i = 0; i < 4; i++)
+        {
+            GameObject fruit = Instantiate(appleObject,
+                                   new Vector3(Random.Range(10, 30), Random.Range(-25, -35), -32));
+        
+            GetComponent<Rigidbody>(fruit).AddForce(throwForce, ForceMode.Impulse);
+        }
+    }
+
+    /// <summary>
+    /// Update this fruit spawner.
+    /// </summary>
+    void Update()
+    {
+		
+    }
+}
